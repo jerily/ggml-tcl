0bb39a0b2761232d8afe8283400b15de79ad199a
Neofytos Dimitriou <neo@jerily.cy>
2024-05-11 08:53:17 -0400

    updated FindTCL.cmake

--- cmake/FindTCL.cmake
+++ cmake/FindTCL.cmake
@@ -32,9 +32,23 @@ library is.  This code sets the following variables:
       and dig from there.
 #]=======================================================================]
 
-set(TCL_POSSIBLE_LIB_PATHS ${TCL_LIBRARY_DIR})
+# if (TCL_LIBRARY_DIR) is set, we assume that the user knows what he is doing
+# and we don't try to find the library in the standard locations
+
+if (TCL_LIBRARY_DIR)
+  set(TCL_POSSIBLE_LIB_PATHS
+          "${TCL_LIBRARY_DIR}"
+  )
+else()
+  set(TCL_POSSIBLE_LIB_PATHS
+          "/usr/local/lib"
+          "/usr/lib"
+  )
+endif()
+
 
 set(TCL_POSSIBLE_LIB_PATH_SUFFIXES
+        tcl9.0
         tcl8.7
         tcl8.6
         tcl8.5
@@ -44,6 +58,7 @@ set(TCL_POSSIBLE_LIB_PATH_SUFFIXES
 find_library(TCL_LIBRARY
   NAMES
   tcl
+  tcl90 tcl9.0 tcl90t tcl9.0t
   tcl87 tcl8.7 tcl87t tcl8.7t
   tcl86 tcl8.6 tcl86t tcl8.6t
   tcl85 tcl8.5
@@ -58,10 +73,13 @@ find_library(TCL_LIBRARY
 
 set(TCL_POSSIBLE_INCLUDE_PATHS
         "${TCL_INCLUDE_DIR}"
-  "${TCL_LIBRARY_PATH_PARENT}/include"
+        "${TCL_LIBRARY_PATH_PARENT}/include"
+        "/usr/local/include"
+        "/usr/include"
   )
 
 set(TCL_POSSIBLE_INCLUDE_PATH_SUFFIXES
+  include/tcl9.0
   include/tcl8.7
   include/tcl8.6
   include/tcl8.5
